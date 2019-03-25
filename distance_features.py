d55f44c12e742edbdc0985c6dca98b07b4c82d5e
klotz94 <simonklotz.sk@gmail.com>
2019-03-25 19:41:02 +0100

    initial commit

---
+++ distance_features.py
@@ -0,0 +1,171 @@
+import pandas as pd
+import numpy as np
+import re
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from gensim.models import KeyedVectors
+from gensim.models.doc2vec import Doc2Vec, Word2Vec, TaggedDocument
+from gensim.models import LsiModel
+from gensim import corpora
+
+from sklearn.metrics.pairwise import paired_cosine_distances, paired_euclidean_distances
+import nltk
+from nltk.corpus import stopwords
+
+# Load data
+train = pd.read_csv("./train.csv", names=['row_ID', 'text_a_ID', 'text_b_ID', 'text_a_text', 'text_b_text', 'have_same_meaning'], index_col=0)
+test = pd.read_csv("./test.csv", names=['row_ID', 'text_a_ID', 'text_b_ID', 'text_a_text', 'text_b_text', 'have_same_meaning'], index_col=0)
+submission_sample = pd.read_csv("./sample_submission_file.csv")
+en_stop = set(stopwords.words('english'))
+glove_file = "./data/word2vec.glove.6B.100d.txt"
+
+
+def clean(q):
+    q = str(q).lower()
+    q = q.replace(",000,000", "m").replace(",000", "k").replace("′", "'").replace("’", "'")                           .replace("won't", "will not").replace("cannot", "can not").replace("can't", "can not")                           .replace("n't", " not").replace("what's", "what is").replace("it's", "it is")                           .replace("'ve", " have").replace("i'm", "i am").replace("'re", " are")                           .replace("he's", "he is").replace("she's", "she is").replace("'s", " own")                           .replace("%", " percent ").replace("₹", " rupee ").replace("$", " dollar ")                           .replace("€", " euro ").replace("'ll", " will")
+    q = re.sub(r"([0-9]+)000000", r"\1m", q)
+    q = re.sub(r"([0-9]+)000", r"\1k", q)
+    return q
+
+# Start computation
+all_questions = pd.concat([train["text_a_text"], train["text_b_text"], test["text_a_text"], test["text_b_text"]])
+question_counts = all_questions.value_counts()
+
+questions = [clean(q) for q in all_questions]
+questions_token = [[w for w in q.split(' ') if w not in en_stop] for q in questions]
+
+
+print("Fit TFIDF Model...")
+tfidf_vectorizer = TfidfVectorizer(ngram_range=(1,2))
+tfidf_vectorizer.fit(all_questions);
+
+print("Load Glove Model...")
+glove_model = KeyedVectors.load_word2vec_format(glove_file)
+
+print("Fit Word2Vec Model...")
+word2Vec = Word2Vec(size=100, window=5, min_count=2, sg=1, workers=10)
+word2Vec.build_vocab(questions_token)  # prepare the model vocabulary
+word2Vec.train(sentences=questions_token, total_examples=len(questions_token), epochs=word2Vec.iter)
+
+print("Fit LSI Model...")
+dictionary = corpora.Dictionary(questions_token)
+corpus = [dictionary.doc2bow(text) for text in questions_token]
+lsi = LsiModel(corpus, num_topics=200, id2word = dictionary)
+
+print("Fit doc2vec Model...")
+q2idx_dict = {tuple(q): idx for idx, q in enumerate(questions_token)}
+
+d2v_training_data = []
+for idx,doc in enumerate(questions_token):
+    d2v_training_data.append(TaggedDocument(doc,[idx]))
+
+d2v_dm = Doc2Vec(d2v_training_data, size=100, window=4, min_count=3, workers=16, iter=5)
+d2v_dm.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
+
+d2v_bow = Doc2Vec(d2v_training_data, size=100, window=4, min_count=3, dm=0, workers=16, iter=5)
+d2v_bow.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
+
+def preprocess(df):
+    df_features = pd.DataFrame(index=df.index)
+    df_intermediate = pd.DataFrame(index=df.index)
+
+    print("--> Compute tokens...")
+    df_intermediate["clean_a"] = df.text_a_text.apply(lambda x: clean(x))
+    df_intermediate["clean_b"] = df.text_b_text.apply(lambda x: clean(x))
+
+    df_intermediate["words_a"] = df_intermediate.apply(lambda row: row.clean_a.split(" "), axis=1)
+    df_intermediate["words_b"] = df_intermediate.apply(lambda row: row.clean_b.split(" "), axis=1)
+
+    df_intermediate["words_clean_a"] = df_intermediate.apply(lambda row: [w for w in row.words_a if w not in en_stop], axis=1)
+    df_intermediate["words_clean_b"] = df_intermediate.apply(lambda row:  [w for w in row.words_b if w not in en_stop], axis=1)
+
+    df_intermediate["stop_a"] = df_intermediate.apply(lambda row: [w for w in row.words_a if w in en_stop], axis=1)
+    df_intermediate["stop_b"] = df_intermediate.apply(lambda row: [w for w in row.words_b if w in en_stop], axis=1)
+    
+    print("--> Compute tfidf distance...")
+    tfidf_a = tfidf_vectorizer.transform(df_intermediate["clean_a"])
+    tfidf_b = tfidf_vectorizer.transform(df_intermediate["clean_b"])
+
+    df_features["tfidf_dist_cosine"] = paired_cosine_distances(tfidf_a, tfidf_b)
+    df_features["tfidf_dist_euclidean"] = paired_euclidean_distances(tfidf_a, tfidf_b)
+    
+    print("--> Compute glove distance...")
+    glove_emb_a = df_intermediate["words_clean_a"].apply(lambda q: np.array([glove_model.wv[w] for w in q if w in glove_model.wv]))
+    glove_emb_b = df_intermediate["words_clean_b"].apply(lambda q: np.array([glove_model.wv[w] for w in q if w in glove_model.wv]))
+
+    glove_emb_a[glove_emb_a.apply(lambda x: len(x)==0)] = glove_emb_a[glove_emb_a.apply(lambda x: len(x)==0)].apply(lambda y: np.zeros((1,100)))
+    glove_emb_b[glove_emb_b.apply(lambda x: len(x)==0)] = glove_emb_b[glove_emb_b.apply(lambda x: len(x)==0)].apply(lambda y: np.zeros((1,100)))
+    glove_emb_a = glove_emb_a.apply(lambda x: np.mean(x,axis=0))
+    glove_emb_b = glove_emb_b.apply(lambda x: np.mean(x,axis=0))
+    glove_emb_a = np.vstack(glove_emb_a.values)
+    glove_emb_b = np.vstack(glove_emb_b.values)
+
+    df_features["glove_dist_cosine"] = paired_cosine_distances(glove_emb_a, glove_emb_b)
+    df_features["glove_dist_euclidean"] = paired_euclidean_distances(glove_emb_a, glove_emb_b)
+    df_features["glove_word_mover_dist"] = df_intermediate.apply(lambda row: glove_model.wv.wmdistance(row["words_clean_a"], row["words_clean_b"]), axis=1)
+    
+    print("--> Compute lsi distance...")
+    lsi_emb_a = df_intermediate["words_clean_a"].apply(lambda x: np.array(lsi[dictionary.doc2bow(x)]))
+    lsi_emb_b = df_intermediate["words_clean_b"].apply(lambda x: np.array(lsi[dictionary.doc2bow(x)]))
+
+    lsi_emb_a[lsi_emb_a.apply(lambda x: len(x)==0 or x.shape[0]!=200)] = lsi_emb_a[lsi_emb_a.apply(lambda x: len(x)==0 or x.shape[0]!=200)].apply(lambda x: np.zeros((200,2)))
+    lsi_emb_b[lsi_emb_b.apply(lambda x: len(x)==0 or x.shape[0]!=200)] = lsi_emb_b[lsi_emb_b.apply(lambda x: len(x)==0 or x.shape[0]!=200)].apply(lambda x: np.zeros((200,2)))
+
+    # Derive question representations from single lsi vectors
+    lsi_emb_a = lsi_emb_a.apply(lambda x: np.mean(x,axis=0))
+    lsi_emb_b = lsi_emb_b.apply(lambda x: np.mean(x,axis=0))
+    lsi_emb_a = np.vstack(lsi_emb_a.values)
+    lsi_emb_b = np.vstack(lsi_emb_b.values)
+
+    df_features["lsi_dist_cosine"] = paired_cosine_distances(lsi_emb_a, lsi_emb_b)
+    df_features["lsi_dist_euclidean"] = paired_euclidean_distances(lsi_emb_a, lsi_emb_b)
+    
+    print("--> Compute word2vec distance...")
+    word2Vec_emb_a = df_intermediate["words_clean_a"].apply(lambda q: np.array([word2Vec.wv[w] for w in q if w in word2Vec.wv]))
+    word2Vec_emb_b = df_intermediate["words_cleWordNetLemmatizeran_b"].apply(lambda q: np.array([word2Vec.wv[w] for w in q if w in word2Vec.wv]))
+
+    word2Vec_emb_a[word2Vec_emb_a.apply(lambda x: len(x)==0)] = word2Vec_emb_a[word2Vec_emb_a.apply(lambda x: len(x)==0)].apply(lambda y: np.zeros((1,100)))
+    word2Vec_emb_b[word2Vec_emb_b.apply(lambda x: len(x)==0)] = word2Vec_emb_b[word2Vec_emb_b.apply(lambda x: len(x)==0)].apply(lambda y: np.zeros((1,100)))
+
+    word2Vec_emb_a = word2Vec_emb_a.apply(lambda x: np.mean(x,axis=0))
+    word2Vec_emb_b = word2Vec_emb_b.apply(lambda x: np.mean(x,axis=0))
+    word2Vec_emb_a = np.vstack(word2Vec_emb_a.values)
+    word2Vec_emb_b = np.vstack(word2Vec_emb_b.values)
+
+    df_features["w2v_dist_cosine"] = paired_cosine_distances(word2Vec_emb_a, word2Vec_emb_b)
+    df_features["w2v_dist_euclidean"] = paired_euclidean_distances(word2Vec_emb_a, word2Vec_emb_b)
+    df_features["word2vec_word_mover_dist"] = df_intermediate.apply(lambda row: word2Vec.wv.wmdistance(row["words_clean_a"], row["words_clean_b"]), axis=1)
+    
+    print("--> Compute doc2vec distance...")
+    doc_vec_dm_emb_a = df_intermediate["words_clean_a"].apply(lambda q: d2v_dm.docvecs[q2idx_dict[tuple(q)]])
+    doc_vec_dm_emb_b = df_intermediate["words_clean_b"].apply(lambda q: d2v_dm.docvecs[q2idx_dict[tuple(q)]])
+    doc_vec_bow_emb_a = df_intermediate["words_clean_a"].apply(lambda q: d2v_bow.docvecs[q2idx_dict[tuple(q)]])
+    doc_vec_bow_emb_b = df_intermediate["words_clean_b"].apply(lambda q: d2v_bow.docvecs[q2idx_dict[tuple(q)]])
+    doc_vec_dm_emb_a = np.vstack(doc_vec_dm_emb_a.values)
+    doc_vec_dm_emb_b = np.vstack(doc_vec_dm_emb_b.values)
+    doc_vec_bow_emb_a = np.vstack(doc_vec_bow_emb_a.values)
+    doc_vec_bow_emb_b = np.vstack(doc_vec_bow_emb_b.values)
+        
+    df_features["dm_dist_cosine"] = paired_cosine_distances(doc_vec_dm_emb_a, doc_vec_dm_emb_b)
+    df_features["dm_dist_euclidean"] = paired_euclidean_distances(doc_vec_dm_emb_a, doc_vec_dm_emb_b)
+    df_features["dm_word_mover_dist"] = df_intermediate.apply(lambda row: d2v_dm.wv.wmdistance(row["words_clean_a"], row["words_clean_b"]), axis=1)
+    
+    df_features["bow_dist_cosine"] = paired_cosine_distances(doc_vec_bow_emb_a, doc_vec_bow_emb_b)
+    df_features["bow_dist_euclidean"] = paired_euclidean_distances(doc_vec_bow_emb_a, doc_vec_bow_emb_b)
+    df_features["bow_word_mover_dist"] = df_intermediate.apply(lambda row: d2v_bow.wv.wmdistance(row["words_clean_a"], row["words_clean_b"]), axis=1)
+    
+    print("--> Compute edit distance...")
+    df_features["edit_distance"] = df_intermediate.apply(lambda x: nltk.edit_distance(x["clean_a"], x["clean_b"]), axis=1)
+        
+    
+    return df_features
+
+
+print("Compute train features...")
+train_features = preprocess(train)
+
+print("Compute test features...")
+test_features = preprocess(test)
+
+train_features.to_csv("distance_features_train.csv", index=False)
+test_features.to_csv("distance_features_test.csv", index=False)
---
+++ graph_features.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+
+import re
+import networkx as nx
+
+# Load data
+train = pd.read_csv("./train.csv", names=['row_ID', 'text_a_ID', 'text_b_ID', 'text_a_text', 'text_b_text', 'have_same_meaning'], index_col=0)
+test = pd.read_csv("./test.csv", names=['row_ID', 'text_a_ID', 'text_b_ID', 'text_a_text', 'text_b_text', 'have_same_meaning'], index_col=0)
+submission_sample = pd.read_csv("./sample_submission_file.csv")
+
+# Hyperparameters
+max_freq = 50
+max_neighbors = 30
+n_k_cores = 11
+max_level = 3
+
+# Start computation
+all_question_ids = pd.concat([train["text_a_ID"], train["text_b_ID"], test["text_a_ID"], test["text_b_ID"]])
+unique_question_ids = all_question_ids.unique()
+
+def shortestPathShortness(row):
+    g.remove_edge(row['text_a_ID'], row['text_b_ID'])
+    try:
+        sps = 1 / nx.shortest_path_length(g, row['text_a_ID'], row['text_b_ID'], weight="weight")
+    except nx.NetworkXNoPath:
+        sps=0
+    g.add_edge(row['text_a_ID'], row['text_b_ID'])
+    return sps
+
+def get_neighbors(qid):
+    neighbors = nx.single_source_shortest_path_length(g, qid, cutoff=2)
+    neighbors_df = pd.DataFrame(list(zip(neighbors.keys(), neighbors.values())), index=neighbors.keys(), columns=["qid", "n_level"])
+
+    neighbors = []
+    for i in range(1, max_level+1):
+        neighbors.append(neighbors_df[neighbors_df.n_level==i].qid.values)
+    return neighbors
+
+
+print("Build Graph...")
+nodes = pd.concat([train.text_a_ID, train.text_b_ID, test.text_a_ID,test.text_b_ID]).values
+edges = pd.concat([train[["text_a_ID", "text_b_ID"]], test[["text_a_ID", "text_b_ID"]]]).values
+
+g = nx.Graph()
+g.add_nodes_from(nodes)
+g.add_edges_from(edges)
+g.remove_edges_from(g.selfloop_edges())
+
+print("Compute question specific features...")
+df_questions = pd.DataFrame(unique_question_ids, columns=["qid"])
+df_questions.index = df_questions.qid
+
+print("--> Compute k cores...")
+df_questions["k_core"] = 0
+for i in range(2,n_k_cores):
+    print("\t--> core {}".format(i))
+    k_core = nx.k_core(g, k=i).nodes()
+    df_questions.loc[df_questions.qid.isin(k_core), "k_core"] = i
+
+print("--> Compute neighbors...")  
+neighbors = df_questions.qid.apply(get_neighbors)
+for i in range(1, max_level+1):
+    df_questions["neighbors" + str(i)] = neighbors.apply(lambda x: set(x[i-1]))
+
+print("--> Compute question frequency...")  
+df_questions["frequency"] = all_question_ids.value_counts()
+
+print("--> Compute page rank...") 
+pageranks = nx.pagerank(g, weight='weight')
+df_questions["page_rank"] = df_questions.qid.apply(lambda qid: pageranks[qid])
+
+print("--> Compute closeness centrality...") 
+closeness_centrality = nx.closeness_centrality(g)
+df_questions["closeness_centrality"] = df_questions.qid.apply(lambda qid: closeness_centrality[qid])
+
+print("--> Compute clustering...") 
+clustering = nx.clustering(g)
+df_questions["clustering"] = df_questions.qid.apply(lambda qid: clustering[qid])
+ 
+print("--> Compute eigenvector centrality...") 
+eigenvector_centrality = nx.eigenvector_centrality(g)
+df_questions["eigenvector_centrality"] = df_questions.qid.apply(lambda qid: eigenvector_centrality[qid])
+
+
+def preprocess(df):
+    df_features = pd.DataFrame(index=df.index)
+    df_intermediate = pd.DataFrame(index=df.index)
+
+    print("--> Compute frequency features...")
+    df_intermediate["freq_a"] = df_questions.loc[df.text_a_ID, "frequency"].values
+    df_intermediate["freq_b"] = df_questions.loc[df.text_b_ID, "frequency"].values
+
+    df_features["frequency_min"] = df_intermediate[["freq_a", "freq_b"]].min(axis=1).apply(lambda x: min(x,max_freq))
+    df_features["frequency_max"] = df_intermediate[["freq_a", "freq_b"]].max(axis=1).apply(lambda x: min(x,max_freq))
+
+    print("--> Compute neighbor features...")
+    for i in range(1, max_level+1):
+        df_intermediate["neighbors_a"] = df_questions.loc[df.text_a_ID, "neighbors" + str(i)].values
+        df_intermediate["neighbors_b"] = df_questions.loc[df.text_b_ID, "neighbors" + str(i)].values
+        df_intermediate["common_neighbors"] = df_intermediate.apply(lambda x: len(list(x.neighbors_a.intersection(x.neighbors_b))), axis=1)
+
+        df_features["common_neighbors" + str(i)] = df_intermediate["common_neighbors"].apply(lambda x: min(x,max_neighbors)).apply(lambda x: min(x,max_neighbors))
+        df_intermediate["min_neighbors"] = df_intermediate[["neighbors_a","neighbors_b"]].apply(lambda x: min(len(x.neighbors_a), len(x.neighbors_b)),axis=1)
+        df_features["common_neighbors_ratio" + str(i)] = df_features["common_neighbors" + str(i)]/(df_intermediate["min_neighbors"] + 0.00001)
+
+    print("--> Compute k-core features...")
+    df_intermediate["k_core_a"] = df_questions.loc[df.text_a_ID, "k_core"].values
+    df_intermediate["k_core_b"] = df_questions.loc[df.text_b_ID, "k_core"].values
+
+    df_features["k_core_min".format(i)] = df_intermediate[["k_core_a", "k_core_b"]].min(axis=1)
+    df_features["k_core_max".format(i)] = df_intermediate[["k_core_a", "k_core_b"]].max(axis=1)
+    
+    print("--> Compute page rank features...")
+    df_intermediate["page_rank_a"] = df_questions.loc[df.text_a_ID, "page_rank"].values
+    df_intermediate["page_rank_b"] = df_questions.loc[df.text_b_ID, "page_rank"].values
+
+    df_features["page_rank_min"] = df_intermediate[["page_rank_a", "page_rank_b"]].min(axis=1).apply(lambda x: min(x,100))
+    df_features["page_rank_max"] = df_intermediate[["page_rank_a", "page_rank_b"]].max(axis=1).apply(lambda x: min(x,100))
+    
+    print("--> Compute closeness centrality features...")
+    df_intermediate["closeness_centrality_a"] = df_questions.loc[df.text_a_ID, "closeness_centrality"].values
+    df_intermediate["closeness_centrality_b"] = df_questions.loc[df.text_b_ID, "closeness_centrality"].values
+
+    df_features["closeness_centrality_min"] = df_intermediate[["closeness_centrality_a", "closeness_centrality_b"]].min(axis=1).apply(lambda x: min(x,100))
+    df_features["closeness_centrality_max"] = df_intermediate[["closeness_centrality_a", "closeness_centrality_b"]].max(axis=1).apply(lambda x: min(x,100))
+    
+    print("--> Compute clustering features...")
+    df_intermediate["clustering_a"] = df_questions.loc[df.text_a_ID, "clustering"].values
+    df_intermediate["clustering_b"] = df_questions.loc[df.text_b_ID, "clustering"].values
+
+    df_features["clustering_min"] = df_intermediate[["clustering_a", "clustering_b"]].min(axis=1).apply(lambda x: min(x,100))
+    df_features["clustering_max"] = df_intermediate[["clustering_a", "clustering_b"]].max(axis=1).apply(lambda x: min(x,100))
+    
+    print("--> Compute eigenvector centrality...")
+    df_intermediate["eigenvector_centrality_a"] = df_questions.loc[df.text_a_ID, "eigenvector_centrality"].values
+    df_intermediate["eigenvector_centrality_b"] = df_questions.loc[df.text_b_ID, "eigenvector_centrality"].values
+
+    df_features["eigenvector_centrality_min"] = df_intermediate[["eigenvector_centrality_a", "eigenvector_centrality_b"]].min(axis=1).apply(lambda x: min(x,100))
+    df_features["eigenvector_centrality_max"] = df_intermediate[["eigenvector_centrality_a", "eigenvector_centrality_b"]].max(axis=1).apply(lambda x: min(x,100))
+    
+    print("--> Compute shortest path shortness...")
+    df_features["shortest_path_shortness"] = df.apply(lambda x: shortestPathShortness(x), axis=1)
+    
+    return df_features
+
+print("Compute train features...")
+train_features = preprocess(train)
+
+print("Compute test features...")
+test_features = preprocess(test)
+
+
+train_features.to_csv("non_nlp_features_train.csv", index=False)
+test_features.to_csv("non_nlp_features_test.csv", index=False)
